fca909a6dbb7435243cb1593f7b6cc779e5b50d9
Utkarsh Singh <165037755+UtkarshSingh-06@users.noreply.github.com>
2025-12-30 23:41:56 +0530

    Delete data directory

--- data/availability_queries.sql
+++
@@ -1,9 +0,0 @@
--- Available rooms between dates
-SELECT *
-FROM rooms
-WHERE room_id NOT IN (
-    SELECT room_id
-    FROM bookings
-    WHERE booking_status = 'CONFIRMED'
-      AND ('2025-01-11' < check_out AND '2025-01-13' > check_in)
-);
